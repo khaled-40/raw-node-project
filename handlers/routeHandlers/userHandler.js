0554f27b542bf1b5e419c47598e6dfeead80f048
khaled-40 <khaledmasud.dev@gmail.com>
2026-03-06 17:57:50 +0600

    implemented PUT and DELETE method for the users to update and delete their information

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -25,6 +25,7 @@ handler.userHandler = (requestProperties, callback) => {
 
 handler._user = {};
 
+// @TODO authentication
 handler._user.post = (requestProperties, callback) => {
     const firstName = typeof (requestProperties.body.firstName) === 'string'
         && requestProperties.body.firstName.trim().length > 0
@@ -83,16 +84,17 @@ handler._user.post = (requestProperties, callback) => {
         console.log(firstName, lastName, password, tosAgreement, phone)
     }
 };
+
 handler._user.get = (requestProperties, callback) => {
     // check if the phone number is valid
-    const phone = typeof (requestProperties.body.phone) === 'string'
-        && requestProperties.body.phone.trim().length === 11
-        ? requestProperties.body.phone
+    const phone = typeof (requestProperties.queryStringObject.phone) === 'string'
+        && requestProperties.queryStringObject.phone.trim().length === 11
+        ? requestProperties.queryStringObject.phone
         : false;
     if (phone) {
         // lookup the user
         data.read('users', phone, (err, u) => {
-            const user = {...parseJSON(u)};
+            const user = { ...parseJSON(u) };
             if (!err && user) {
                 delete user.password;
                 callback(200, user)
@@ -108,11 +110,109 @@ handler._user.get = (requestProperties, callback) => {
         })
     }
 };
+
+// @TODO authentication
 handler._user.put = (requestProperties, callback) => {
+    // check if the phone number is valid
+    const phone = typeof (requestProperties.body.phone) === 'string'
+        && requestProperties.body.phone.trim().length === 11
+        ? requestProperties.body.phone
+        : false;
+    if (phone) {
+        const firstName = typeof (requestProperties.body.firstName) === 'string'
+            && requestProperties.body.firstName.trim().length > 0
+            ? requestProperties.body.firstName
+            : false;
+        const lastName = typeof (requestProperties.body.lastName) === 'string'
+            && requestProperties.body.lastName.trim().length > 0
+            ? requestProperties.body.lastName
+            : false;
+        const phone = typeof (requestProperties.body.phone) === 'string'
+            && requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
+        const password = typeof (requestProperties.body.password) === 'string'
+            && requestProperties.body.password.trim().length > 0
+            ? requestProperties.body.password
+            : false;
+        if (firstName || lastName || password) {
+            // lookup the user
+            data.read('users', phone, (err, uData) => {
+                const userData = { ...parseJSON(uData) }
+                if (!err && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if (password) {
+                        userData.password = hash(password);
+                    }
 
+                    // update in the database
+                    data.update('users', phone, userData, (err2) => {
+                        if (!err2) {
+                            callback(200, {
+                                "message": "User updated Successfully"
+                            })
+                        } else {
+                            callback(500, {
+                                error: "There was a problem in the serverside"
+                            })
+                        }
+                    })
+                } else {
+                    callback(400, {
+                        error: 'You have a problem in your request. Could not find the user'
+                    })
+                }
+            })
+        } else {
+            callback(400, {
+                error: 'You have a problem in your request'
+            })
+        }
+    } else {
+        callback(400, {
+            error: 'The phone number is invalid'
+        })
+    }
 };
-handler._user.delete = (requestProperties, callback) => {
 
+// @TODO authentication
+handler._user.delete = (requestProperties, callback) => {
+    // check if the phone number is valid
+    const phone = typeof (requestProperties.queryStringObject.phone) === 'string'
+        && requestProperties.queryStringObject.phone.trim().length === 11
+        ? requestProperties.queryStringObject.phone
+        : false;
+    if (phone) {
+        // lookup the user
+        data.read('users', phone, (err, userData) => {
+            if (!err && userData) {
+                data.delete('users', phone, (err1) => {
+                    if (!err1) {
+                        callback(200, {
+                            "message": "user deleted successfully"
+                        })
+                    } else {
+                        callback(500, {
+                            error: 'There was a server side error'
+                        })
+                    }
+                })
+            } else {
+                callback(404, {
+                    error: 'could not find the user to delete!'
+                })
+            }
+        })
+    } else {
+        callback(400, {
+            error: 'The phone number is invalid'
+        })
+    }
 };
 
 
